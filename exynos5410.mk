a88dba2c582a38d4c2516fb5515c46c820589530
Dracanrage <ljn312902918@gmail.com>
2018-07-05 23:44:35 +0800

    exynos5410-common:Use gpsd and new gps.h from exynos5420.

--- exynos5410.mk
+++ exynos5410.mk
@@ -114,6 +114,10 @@ PRODUCT_PACKAGES += \
     pvrsrvctl \
     libcorkscrew
 
+#GPS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl
+
 # Graphics
 PRODUCT_PACKAGES += \
     hwcomposer.exynos5 \
